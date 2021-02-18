7fd1c4ec19081ddcf1fa926ebb0df23266187459
okutasan <oktavanhowen@gmail.com>
2021-02-19 03:28:18 +0700

    add upgrade minikube

---
+++ minikube/upgrade-minikube.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# Minikube update script file
+
+minikube delete && \ 
+sudo rm -rf /usr/local/bin/minikube && \ 
+sudo curl -Lo minikube https://github.com/kubernetes/minikube/releases/download/v1.17.1/minikube-linux-amd64 && \ 
+sudo chmod +x minikube && \ 
+sudo cp minikube /usr/local/bin/ && \ 
+sudo rm minikube && \  
+minikube start &&\
+
+# Enabling addons: ingress, dashboard
+minikube addons enable ingress && \
+minikube addons enable dashboard && \
+minikube addons enable metrics-server && \
+# Showing enabled addons
+echo '\n\n\033[4;33m Enabled Addons \033[0m' && \
+minikube addons list | grep STATUS && minikube addons list | grep enabled && \
+
+# Showing current status of Minikube
+echo '\n\n\033[4;33m Current status of Minikube \033[0m' && minikube status
+
